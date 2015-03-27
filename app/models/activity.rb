e2ef6dca785684465749bf9111a79bbd6208d2ae
Anonyo Hossain <anonyo.hossain@gmail.com>
2015-03-26 22:24:03 -0400

    Adds scope to count activities by date

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,4 +1,8 @@
 class Activity < ActiveRecord::Base
   has_many :categories
   default_scope { order("created_at DESC") }
+
+  def self.by_date_and_category(date, category)
+    where("created_at >= ?", date).where(category_title: category)
+  end
 end
