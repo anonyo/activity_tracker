99f06cb3e685bd5bfe67da720f4020936a089bab
Anonyo Hossain <anonyo.hossain@gmail.com>
2015-04-18 11:41:46 -0400

    Sorts activities by start_time

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,6 +1,6 @@
 class Activity < ActiveRecord::Base
   has_many :categories
-  default_scope { order("created_at DESC") }
+  default_scope { order("start_time DESC") }
 
   def self.by_date_and_category(date, category)
     where("created_at >= ?", date).where(category_title: category)
